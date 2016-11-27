1eed339346cdcd21ff3d3c175080526509965c2a
Sahil Bajaj <sahil29@gmail.com>
2016-11-27 20:36:29 +0800

    Prune commajump file if found directory no longer exists

--- ,.fish
+++ ,.fish
@@ -8,5 +8,9 @@ function , --description 'Commajump'
 
 	set found_directory (printf '%s\n' $found_paths | uniq -c | sort -nr | head -1 | tr -s ' ' | cut -d' ' -f3-)
 
+	if not test -z $found_directory; and not test -d $found_directory
+		grep -v \^$found_directory\$ ~/.commajump > ~/.commajump.temp; mv ~/.commajump{.temp,}; and echo Nonexistent path $found_directory pruned.
+	end
+
 	cd $found_directory ^ /dev/null; or echo No directories matching "'$argv'" found.
 end
