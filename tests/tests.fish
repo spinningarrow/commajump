74e8c1e36af5aa71aa76bd7d91c72e43a68990df
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2024-04-25 18:34:43 +0200

    Update tests
    A lot has changed in fishtape

--- tests/tests.fish
+++ tests/tests.fish
@@ -1,32 +1,21 @@
 function commatest
-	set -x COMMAJUMP_PATH (pwd)/tests/commajumpfile
-	fish -c "functions -e __commajump_add; source "(pwd)"/,.fish; , $argv; pwd"
+	fish -c "set -x COMMAJUMP_PATH (pwd)/tests/commajumpfile; functions -e __commajump_add; source "(pwd)"/,.fish; , $argv; pwd" | tail -1
 end
 
-function setup
-	sed s:@pwd@:(pwd): (pwd)/tests/commajumpfile.original > (pwd)/tests/commajumpfile
-end
+# setup
+sed s:@pwd@:(pwd): (pwd)/tests/commajumpfile.original > (pwd)/tests/commajumpfile
+set dir (dirname (status --current-filename))
 
-function teardown
-	rm (pwd)/tests/commajumpfile
-end
+@test "it does not change the directory when directory is not known to commajump" (commatest unknown) = (pwd)
 
-test "it changes to a directory matching the query"
-	"$DIRNAME/dirs/foo" = (commatest foo)
-end
+@test "it changes to a directory matching the query" (commatest foo) = "$dir/dirs/foo"
 
-test "it does not change the directory when directory is not known to commajump"
-	(pwd) = (commatest unknown)
-end
+@test "it changes to the more-often used directory when there are multiple matches" (commatest ba) = "$dir/dirs/baz"
 
-test "it changes to the more-often used directory when there are multiple matches"
-	"$DIRNAME/dirs/baz" = (commatest ba)
-end
+@test "it matches the path if no known folder names match" (commatest moe) = "$dir/dirs/larry/moe/curly"
 
-test "it matches the path if no known folder names match"
-	"$DIRNAME/dirs/larry/moe/curly" = (commatest moe)
-end
+commatest nonexistent > /dev/null
+@test "it removes nonexistent directories" (grep -q nonexistent $dir/commajumpfile) $status -eq 1
 
-test "it removes nonexistent directories"
-	"$DIRNAME/dirs/nonexistent" != (commatest nonexistent; cat $DIRNAME/commajumpfile)
-end
+# teardown
+rm (pwd)/tests/commajumpfile
